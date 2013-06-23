c6c640f7c19dcf8c7db4007494f21af185804252
JJ <JJ@10.0.0.36>
2013-06-23 23:11:59 +0100

    REFACTOR. Inline And

--- src/Translator.java
+++ src/Translator.java
@@ -1,19 +1,16 @@
 public class Translator {
 
 	public static String translate(int n) {
-		return	(words(n / 1000000, " million" + and(n % 1000000)) + 
-			words((n % 1000000) / 1000, " thousand" + and(n % 1000)) + words(n % 1000, "")).replace("  ", " ").trim();
-	}
-
-	private static String and(int number) {
-		return (number > 0) && (number < 100) ? " and " : " ";
+		return	(words(n / 1000000, " million" + ((n % 1000000 > 0) && (n % 1000000 < 100) ? " and " : " ")) + 
+			words((n % 1000000) / 1000, " thousand" + ((n % 1000 > 0) && (n % 1000 < 100) ? " and " : " ")) + 
+			words(n % 1000, "")).replace("  ", " ").trim();
 	}
 
 	private static String words(int n, String ending) {
 		String[] numbers = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
 			"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
 		String[] tenSet = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-		return (n == 0) ? "" : ((n >= 100) ? numbers[n / 100] + " hundred" + and(n % 100) : "") + 
+		return (n == 0) ? "" : ((n >= 100) ? numbers[n / 100] + " hundred" + ((n % 100 > 0) && (n % 100 < 100) ? " and " : " ") : "") + 
 			   (((n % 100) >= 20) ? tenSet[(n % 100) / 10] + " " + numbers[n % 10] : numbers[n % 20]) + ending;
 	}
 }
