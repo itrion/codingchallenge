6f39266afc22d3b5569861b31e9f38003bfbbfd0
JJ <JJ@10.0.0.36>
2013-06-23 20:52:18 +0100

    GREEN. OneThousandToNineThousand

--- src/Translator.java
+++ src/Translator.java
@@ -1,12 +1,16 @@
 public class Translator {
 
 	public static String translate(int number) {
-		String[] numbers = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-		return ((number >= 100) ? numbers[number / 100] + " hundred" + and(number) + numbers[number %100]: numbers[number]);
+		return (number >= 1000) ? words(number /1000) + " thousand" : words(number);
 	}
 
 	private static String and(int number) {
 		return (number %100 > 0) ? " and " : "";
+	}
+
+	private static String words(int number) {
+		String[] numbers = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+		return ((number >= 100) ? numbers[number / 100] + " hundred" + and(number) + numbers[number %100]: numbers[number]);
 	}
 
 }
