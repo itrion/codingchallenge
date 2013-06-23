497db94c78e59d4ec9bb29216168d728ef6d49dd
JJ <JJ@10.0.0.36>
2013-06-23 20:40:08 +0100

    RED. OneToNine

--- test/TranslatorTest.java
+++ test/TranslatorTest.java
@@ -1,15 +1,21 @@
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TranslatorTest {
 
-    public TranslatorTest() {
-    }
-
 	@Test
-	public void testSomeMethod() {
-		fail("The test case is a prototype.");
+	public void oneToNine() {
+		Assert.assertEquals("one", Translator.translate(1));
+		Assert.assertEquals("two", Translator.translate(2));
+		Assert.assertEquals("three", Translator.translate(3));
+		Assert.assertEquals("four", Translator.translate(4));
+		Assert.assertEquals("five", Translator.translate(5));
+		Assert.assertEquals("six", Translator.translate(6));
+		Assert.assertEquals("seven", Translator.translate(7));
+		Assert.assertEquals("eight", Translator.translate(8));
+		Assert.assertEquals("nine", Translator.translate(9));
 	}
 
 }
