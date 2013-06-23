2e42050a90738b30c3ce2543707e0b7fe0f7ff45
JJ <JJ@10.0.0.36>
2013-06-23 20:49:14 +0100

    REFACTOR

--- src/Translator.java
+++ src/Translator.java
@@ -2,7 +2,11 @@ public class Translator {
 
 	public static String translate(int number) {
 		String[] numbers = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-		return ((number >= 100) ? numbers[number / 100] + " hundred" + ((number %100 >0) ? " and " : "") + numbers[number %100]: numbers[number]);
+		return ((number >= 100) ? numbers[number / 100] + " hundred" + and(number) + numbers[number %100]: numbers[number]);
+	}
+
+	private static String and(int number) {
+		return (number %100 > 0) ? " and " : "";
 	}
 
 }
