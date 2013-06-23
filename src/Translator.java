a060b773a1546cb0a4fbcf4390915f987efd6584
JJ <JJ@10.0.0.36>
2013-06-23 20:48:29 +0100

    GREEN. OneHundredAndOneToNineHundredAndNine

--- src/Translator.java
+++ src/Translator.java
@@ -2,7 +2,7 @@ public class Translator {
 
 	public static String translate(int number) {
 		String[] numbers = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-		return ((number >= 100) ? numbers[number / 100] + " hundred" : numbers[number]);
+		return ((number >= 100) ? numbers[number / 100] + " hundred" + ((number %100 >0) ? " and " : "") + numbers[number %100]: numbers[number]);
 	}
 
 }
