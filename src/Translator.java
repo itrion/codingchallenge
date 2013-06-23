b04358f016e4aacd5833ba43a014a225a15afe09
JJ <JJ@10.0.0.36>
2013-06-23 21:30:23 +0100

    GREEN. millionThousandAndNumber

--- src/Translator.java
+++ src/Translator.java
@@ -1,17 +1,18 @@
 public class Translator {
 
 	public static String translate(int number) {
-		return	(number >= 1000000) ? words(number / 1000000) + " million" + and(number % 1000) + words(number % 1000) :
-				(number >= 1000) ? words(number / 1000) + " thousand" + and(number % 1000) + words(number % 1000) : 
-				(words(number));
+		return	(words(number / 1000000, " million" + and(number % 1000000)) + 
+				words((number % 1000000) / 1000, " thousand" + and(number % 1000)) + 
+				words(number % 1000, "")).replace("  ", " ").trim();
 	}
 
 	private static String and(int number) {
-		return (number % 100 > 0) ? " and " : "";
+		return (number > 0) && (number < 100) ? " and " : " ";
 	}
 
-	private static String words(int number) {
+	private static String words(int number, String ending) {
+		if (number == 0) return "";
 		String[] numbers = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-		return ((number >= 100) ? numbers[number / 100] + " hundred" + and(number) + numbers[number % 100] : numbers[number]);
+		return ((number >= 100) ? numbers[number / 100] + " hundred" + and(number % 100) + numbers[number % 100] : numbers[number]) + ending;
 	}
 }
